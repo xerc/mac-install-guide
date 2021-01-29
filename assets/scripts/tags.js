f8bdd7795f8f04f773bf59fc6032f9b2210b74fb
Daniel Kehoe <daniel@danielkehoe.com>
2021-01-29 09:16:16 +0000

    load all tags with one script

---
+++ assets/scripts/tags.js
@@ -0,0 +1,20 @@
+import { InstallguideAd } from './installguide-ad.js';
+import { InstallguideFooter } from './installguide-footer.js';
+import { InstallguideNavbar } from './installguide-navbar.js';
+import { YaxAuthorKehoe } from './yax-author-kehoe.js';
+import { YaxMarkdown } from './yax-markdown.js';
+import { YaxTutorialHero } from './yax-tutorial-hero.js';
+import { YaxTutorialPaginate } from './yax-tutorial-paginate.js';
+import { YaxTutorialToc } from './yax-tutorial-toc.js';
+const YaxTags = {
+  InstallguideAd,
+  InstallguideFooter,
+  InstallguideNavbar,
+  YaxAuthorKehoe,
+  YaxMarkdown,
+  YaxTutorialHero,
+  YaxTutorialPaginate,
+  YaxTutorialToc
+};
+
+export { YaxTags };
